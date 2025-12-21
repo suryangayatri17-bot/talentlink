de84a0b5fc9027a56ad13cc06003eb05526ae830
Gayathri <suryangayatri17@gmail.com>
2025-12-21 11:02:13 +0530

    Fixed: Only including core freelancer dashboard files

--- freelancer_dashboard/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, Proposal, Contract
-
-admin.site.register(Project)
-admin.site.register(Proposal)
-admin.site.register(Contract)
--- freelancer_dashboard/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FreelancerDashboardConfig(AppConfig):
-    name = 'freelancer_dashboard'
